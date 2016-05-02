b8deca5cf4f8232717699abde3bd21ffc8fb5342
Clayton Ritcher <claytonritcher@gmail.com>
2016-05-02 16:05:34 -0400

    First try at resizing an image in cuda.

--- src/cudaCompressor.cu
+++ src/cudaCompressor.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 
 #include <cuda.h>
 #include <cuda_runtime.h>
@@ -8,6 +9,8 @@
 #include "image.h"
 #include "ppm.h"
 #include "compressedFile.h"
+
+#include "imageUtils.cu_inl"
 
 struct GlobalConstants {
     int imageWidth;
@@ -25,6 +28,19 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
       fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
       if (abort) exit(code);
    }
+}
+
+__global__ void resizeKernel(int* resizedImg, float scale, int w, int h) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int x = index % deviceConstants.imageWidth;
+    int y = index / deviceConstants.imageWidth;
+
+    int r, g, b, a;
+    int oldX = scale * x;
+    int oldY = scale * y;
+
+    pixelGet(oldX, oldY, deviceConstants.imageWidth, deviceConstants.imageHeight, &r, &g, &b, &a, deviceConstants.imageData);
+    pixelSet(x, y, w, h, r, g, b, a, resizedImg);
 }
 
 CudaCompressor::CudaCompressor(const std::string& imageFilename, int rangeSize, int domainSize) {
@@ -50,7 +66,24 @@ CudaCompressor::~CudaCompressor() {
 }
 
 void CudaCompressor::compress() {
+  if (!image || image->width % compIm.rangeSize || image->height % compIm.rangeSize ||
+      image->width % compIm.domainSize || image->height % compIm.domainSize) {
+    std::cerr << "Invalid compression request" << std::endl;
+    return;
+  }
+  dim3 rangeDim(compIm.rangeSize * compIm.rangeSize, 1);
 
+  // Get resized image
+  int* smallImg;
+  float scale = ((float) compIm.domainSize) / compIm.rangeSize;
+  int newW = image->width / scale;
+  int newH = image->height / scale;
+  
+  cudaMalloc(&(smallImg), sizeof(int) * 4 * newW * newH);
+
+  dim3 resizeDim((newW * newH) / rangeDim.x);
+  circleChunkTestKernel<<<resizeDim, rangeDim>>>(smallImg, scale, newW, newH);
+  cudaThreadSynchronize();
 }
 
 void CudaCompressor::saveToFile(const std::string& filename) {
